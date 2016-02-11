8e4bdf1fd6303f310ecb64ee164a503ecb4f7ee4
Shay <hallshay@gmail.com>
2016-02-10 22:32:03 -0500

    Touch ups all around.  Plus scrolling.

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,29 @@
-// $(function(){
-//  window.setInterval(function(){
-//    $('.name').toggleClass('gone');
-//  }, 2000);
-// });
+var headerHeight = $("#header").height();
+
+$('a[href="#about"]').on("click", function(e) {
+    e.preventDefault();
+  var target = $(this).attr("href");
+  var scrollToPosition = $(target).offset().top - headerHeight;
+  $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000);
+});
+
+$('a[href="#resume"]').on("click", function(e) {
+    e.preventDefault();
+  var target = $(this).attr("href");
+  var scrollToPosition = $(target).offset().top - headerHeight;
+  $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000);
+});
+
+$('a[href="#portfolio"]').on("click", function(e) {
+    e.preventDefault();
+  var target = $(this).attr("href");
+  var scrollToPosition = $(target).offset().top - headerHeight;
+  $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000);
+});
+
+$('a[href="#contact"]').on("click", function(e) {
+    e.preventDefault();
+  var target = $(this).attr("href");
+  var scrollToPosition = $(target).offset().top - headerHeight;
+  $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000);
+});
