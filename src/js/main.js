e9ee1c25e40f9cd44c8026f7a027b9405f8b14f2
Shay <hallshay@gmail.com>
2016-02-15 12:02:55 -0500

    Hide Resume details until title is clicked.

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,21 @@ $('a[href="#contact"]').on("click", function(e) {
   var scrollToPosition = $(target).offset().top - headerHeight;
   $('html,body').animate({ 'scrollTop': scrollToPosition }, 1000);
 });
+
+$('a[href="#technical"]').on("click", function(e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $(target).toggleClass("hidden");
+});
+
+$('a[href="#education"]').on("click", function(e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $(target).toggleClass("hidden");
+});
+
+$('a[href="#experience"]').on("click", function(e) {
+    e.preventDefault();
+    var target = $(this).attr("href");
+    $(target).toggleClass("hidden");
+});
